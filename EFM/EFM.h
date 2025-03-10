e2fe9513848700999be71b1560517cfd4f2ab909
Dennis Yaremenko <yaremdennis@gmail.com>
2025-03-10 09:31:50 +0200

    Initial Commit

---
+++ EFM/EFM.h
@@ -0,0 +1,18 @@
+//
+//  EFM.h
+//  EFM
+//
+//  Created by Denis Yaremenko on 10.03.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EFM.
+FOUNDATION_EXPORT double EFMVersionNumber;
+
+//! Project version string for EFM.
+FOUNDATION_EXPORT const unsigned char EFMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EFM/PublicHeader.h>
+
+
